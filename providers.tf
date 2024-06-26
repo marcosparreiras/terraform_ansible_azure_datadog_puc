55cc992a2c20d73a1fab43fe9d1413749149aafe
Marcos Parreiras <marcosalp1998@gmail.com>
2024-06-25 22:12:58 -0300

    init: start a terraform project with azure provider

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.109.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+  subscription_id = var.az_subscription_id
+  tenant_id       = var.az_tenant_id
+  client_id       = var.az_client_id
+  client_secret   = var.az_client_secret
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "az_subscription_id" {
+  type        = string
+  description = "azure subscription id"
+}
+
+variable "az_tenant_id" {
+  type        = string
+  description = "azure tenant id"
+}
+
+variable "az_client_id" {
+  type        = string
+  description = "azure client id"
+}
+
+variable "az_client_secret" {
+  type        = string
+  description = "azure clinet secret"
+}
