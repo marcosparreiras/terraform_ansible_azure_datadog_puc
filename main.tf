4e65cefe0d0cf145f1fe0acf8768296c427a8ba3
Marcos Parreiras <marcosalp1998@gmail.com>
2024-06-25 23:15:55 -0300

    fix: a typo found on plan

--- main.tf
+++ main.tf
@@ -131,6 +131,6 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_network_interface_security_group_association" "nsg_association" {
   count                      = 2
-  network_interface_id       = azurerm_network_interface.nic[count.index].id
+  network_interface_id       = azurerm_network_interface.vm_nic[count.index].id
   network_security_group_id  = azurerm_network_security_group.nsg.id
 }
